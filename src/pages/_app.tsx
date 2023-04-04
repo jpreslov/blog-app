b0fb8e9225520596df0a47472b0d88f000eb8549
Jake <lovett_jp@outlook.com>
2023-04-04 18:29:20 -0500

    🤬🤬🤬

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,17 +2,21 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Nav from '../components/Nav'
 
-import { ClerkProvider, MultisessionAppSupport, RedirectToSignUp, SignedIn, SignedOut, useAuth } from '@clerk/nextjs'
+import { ClerkProvider, SignedIn, SignedOut } from '@clerk/nextjs'
 import { dark } from '@clerk/themes';
 import SignUpPage from './sign-up/[[...index]]';
-
-// export { reportWebVitals } from 'next-axiom';
+// import SignUpPage from './sign-up/[[...index]]';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ClerkProvider {...pageProps} appearance={{ baseTheme: dark }}>
-      <Nav />
-      <Component {...pageProps} />
+      <SignedIn>
+        <Nav />
+        <Component {...pageProps} />
+      </SignedIn>
+      <SignedOut>
+        <SignUpPage />
+      </SignedOut>
     </ClerkProvider>
   )
 }
