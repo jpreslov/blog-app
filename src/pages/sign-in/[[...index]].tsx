fa2cfcb441a51b6c393d56fadd41df072399a545
Jake <lovett_jp@outlook.com>
2023-04-04 23:49:54 -0500

    🛡🛡🛡

--- src/pages/sign-in/[[...index]].tsx
+++ src/pages/sign-in/[[...index]].tsx
@@ -1,11 +1,13 @@
-import { SignIn } from '@clerk/nextjs'
+import { SignedOut, SignIn } from '@clerk/nextjs'
 
 const SignInPage = () => {
 
   return (
-    <div className='flex content-center justify-center mt-32'>
-      <SignIn path='/sign-in' routing='path' signUpUrl='/sign-up' />
-    </div>
+    <SignedOut>
+      <div className='flex content-center justify-center mt-32'>
+        <SignIn path='/sign-in' routing='path' signUpUrl='/sign-up' redirectUrl='/' />
+      </div>
+    </SignedOut>
   )
 }
 
--- src/pages/sign-up/[[...index]].tsx
+++ src/pages/sign-up/[[...index]].tsx
@@ -1,11 +1,13 @@
-import { SignUp } from '@clerk/nextjs'
+import { SignUp, SignedOut } from '@clerk/nextjs'
 
 const SignUpPage = () => {
 
   return (
-    <div className='flex content-center justify-center mt-32'>
-      <SignUp path='/sign-up' routing='path' signInUrl='/sign-in' />
-    </div>
+    <SignedOut>
+      <div className='flex content-center justify-center mt-32'>
+        <SignUp path='/sign-up' routing='path' signInUrl='/sign-in' redirectUrl='/' />
+      </div>
+    </SignedOut>
   )
 }
 
